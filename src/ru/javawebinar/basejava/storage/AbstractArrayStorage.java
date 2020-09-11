332eda494131f64498da3a62b1ade2645fa1d00c
SemenRyzhkov <thaisun2018@yandex.ru>
2020-09-11 09:14:23 +0300

    Homework3.1

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -15,6 +15,38 @@ public abstract class AbstractArrayStorage implements Storage {
         size = 0;
     }
 
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+
+        if (index < 0) {
+            System.out.println("Резюме с таким uuid не существует");
+        } else {
+            storage[index] = resume;
+            System.out.println("Резюме c uuid " + resume.getUuid() + " успешно обновлено");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Резюме с таким uuid не существует");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+
+        if (index < 0) {
+            System.out.println("Резюме с таким uuid не существует");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
     /**
      * @return array, contains only Resumes in storage (without null)
      */
--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -7,17 +7,6 @@ import ru.javawebinar.basejava.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage{
 
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-
-        if (index == -1) {
-            System.out.println("Резюме с таким uuid не существует");
-        } else {
-            storage[index] = resume;
-            System.out.println("Резюме c uuid " + resume.getUuid() + " успешно обновлено");
-        }
-    }
-
     public void save(Resume resume) {
 
         if (getIndex(resume.getUuid()) != -1) {
@@ -29,27 +18,6 @@ public class ArrayStorage extends AbstractArrayStorage{
         } else {
             storage[size] = resume;
             size++;
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Резюме с таким uuid не существует");
-            return null;
-        }
-        return storage[index];
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-
-        if (index == -1) {
-            System.out.println("Резюме с таким uuid не существует");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
         }
     }
 
