31cfd0b8268cda71aed54577b1d73c1b72442815
SemenRyzhkov <thaisun2018@yandex.ru>
2020-10-17 12:48:48 +0300

    HW10addJSON XML

--- src/ru/javawebinar/basejava/storage/strategy/BiSerializeConsumer.java
+++
@@ -1,7 +0,0 @@
-package ru.javawebinar.basejava.storage.strategy;
-
-import java.io.IOException;
-
-public interface BiSerializeConsumer<K, V>{
-    void accept(K k, V v) throws IOException;
-}
--- src/ru/javawebinar/basejava/storage/strategy/DataSerializeStrategy.java
+++ src/ru/javawebinar/basejava/storage/strategy/DataSerializeStrategy.java
@@ -14,42 +14,35 @@ public class DataSerializeStrategy implements SerializeStrategy {
             dos.writeUTF(resume.getFullName());
             Map<ContactType, String> contacts = resume.getContacts();
             dos.writeInt(contacts.size());
-//            for (Map.Entry<ContactType, String> pair : contacts.entrySet()) {
-//                dos.writeUTF(pair.getKey().name());
-//                dos.writeUTF(pair.getValue());
-//            }
-            writeWithException(contacts, (k, v) ->
-            {
-                dos.writeUTF(k.name());
-                dos.writeUTF(v);
-            });
+            for (Map.Entry<ContactType, String> pair : contacts.entrySet()) {
+                dos.writeUTF(pair.getKey().name());
+                dos.writeUTF(pair.getValue());
+            }
 
             Map<SectionType, AbstractSection> sectionMap = resume.getSections();
             dos.writeInt(sectionMap.size());
-            writeWithException(sectionMap, (k, v) ->
-            {
-                switch (k) {
+            for (Map.Entry<SectionType, AbstractSection> pair : sectionMap.entrySet()) {
+                SectionType sectionType = pair.getKey();
+
+                switch (sectionType) {
                     case PERSONAL, OBJECTIVE -> {
-                        dos.writeUTF(k.name());
-                        dos.writeUTF(((TextSection) v).getContent());
+                        dos.writeUTF(pair.getKey().name());
+                        dos.writeUTF(((TextSection) pair.getValue()).getContent());
                     }
                     case ACHIEVEMENT, QUALIFICATIONS -> {
-                        dos.writeUTF(k.name());
-                        List<String> list = ((TextListSection) v).getList();
-                        dos.writeInt(list.size());
-                        writeWithException(list, dos::writeUTF);
+                        dos.writeUTF(pair.getKey().name());
+                        List<String> list = ((TextListSection) pair.getValue()).getList();
+                        writeWithException(list, dos, dos::writeUTF);
                     }
                     case EXPERIENCE, EDUCATION -> {
-                        dos.writeUTF(k.name());
-                        List<Organization> orgList = ((OrganizationListSection) v).getOrganizationList();
-                        dos.writeInt(orgList.size());
-                        writeWithException(orgList, e ->
+                        dos.writeUTF(pair.getKey().name());
+                        List<Organization> orgList = ((OrganizationListSection) pair.getValue()).getOrganizationList();
+                        writeWithException(orgList, dos, e ->
                         {
                             dos.writeUTF(e.getHomePage().getName());
                             dos.writeUTF(e.getHomePage().getUrl());
                             List<Organization.Experience> expList = e.getExperienceList();
-                            dos.writeInt(expList.size());
-                            writeWithException(expList, x -> {
+                            writeWithException(expList, dos, x -> {
                                         dos.writeUTF(x.getStartTime().toString());
                                         dos.writeUTF(x.getEndTime().toString());
                                         dos.writeUTF(x.getTitle());
@@ -61,44 +54,7 @@ public class DataSerializeStrategy implements SerializeStrategy {
                         });
                     }
                 }
-            });
-//            for (Map.Entry<SectionType, AbstractSection> pair : sectionMap.entrySet()) {
-//                SectionType sectionType = pair.getKey();
-//                switch (sectionType) {
-//                    case PERSONAL, OBJECTIVE -> {
-//                        dos.writeUTF(pair.getKey().name());
-//                        dos.writeUTF(((TextSection) pair.getValue()).getContent());
-//                    }
-//                    case ACHIEVEMENT, QUALIFICATIONS -> {
-//                        dos.writeUTF(pair.getKey().name());
-//                        List<String> list = ((TextListSection) pair.getValue()).getList();
-//                        dos.writeInt(list.size());
-//                        for (String str : list) {
-//                            dos.writeUTF(str);
-//                        }
-//                    }
-//                    case EXPERIENCE, EDUCATION -> {
-//                        dos.writeUTF(pair.getKey().name());
-//                        List<Organization> organizations = ((OrganizationListSection) pair.getValue())
-//                                .getOrganizationList();
-//                        dos.writeInt(organizations.size());
-//                        for (Organization e : organizations) {
-//                            dos.writeUTF(e.getHomePage().getName());
-//                            dos.writeUTF(e.getHomePage().getUrl());
-//                            List<Organization.Experience> experiences = e.getExperienceList();
-//                            dos.writeInt(experiences.size());
-//                            for (Organization.Experience exp : experiences) {
-//                                dos.writeUTF(exp.getStartTime().toString());
-//                                dos.writeUTF(exp.getEndTime().toString());
-//                                dos.writeUTF(exp.getTitle());
-//                                if (exp.getDescription() != null) {
-//                                    dos.writeUTF(exp.getDescription());
-//                                } else dos.writeUTF("null");
-//                            }
-//                        }
-//                    }
-//                }
-//            }
+            }
         }
     }
 
@@ -156,27 +112,12 @@ public class DataSerializeStrategy implements SerializeStrategy {
         }
     }
 
-    private static <T> void writeWithException(Collection<T> collection,
+    private static <T> void writeWithException(Collection<T> collection, DataOutputStream dos,
                                                SerializeConsumer<T> consumer) throws IOException {
         Objects.requireNonNull(consumer);
+        dos.writeInt(collection.size());
         for (T t : collection) {
             consumer.accept(t);
-        }
-    }
-
-    private static <K, V> void writeWithException(Map<K, V> map,
-                                                  BiSerializeConsumer<K, V> consumer) throws IOException {
-        Objects.requireNonNull(consumer);
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            K k;
-            V v;
-            try {
-                k = entry.getKey();
-                v = entry.getValue();
-            } catch (IllegalStateException ise) {
-                throw new ConcurrentModificationException(ise);
-            }
-            consumer.accept(k, v);
         }
     }
 }
