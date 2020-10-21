ab8e80af5ad43145c728ea19ea5ae55593d7a1cf
SemenRyzhkov <thaisun2018@yandex.ru>
2020-10-21 18:15:42 +0300

    HW12addJavaStreamEx

--- src/ru/javawebinar/basejava/DeadLockEx.java
+++ src/ru/javawebinar/basejava/DeadLockEx.java
@@ -9,13 +9,18 @@ public class DeadLockEx {
         new Thread(()->lock(LOCK1, LOCK2)).start();
     }
 
-    public static void lock(Object lock1, Object lock2){
-        System.out.println("Попытка захватить монитор " + lock1);
+    public static void lock(Object lock1, Object lock2) {
+        System.out.println(Thread.currentThread().getName() + ": " + "Попытка захватить монитор " + lock1);
         synchronized (lock1){
-            System.out.println("Монитор " + lock1 + " захвачен");
-            System.out.println("Попытка захватить монитор " + lock2);
+            System.out.println(Thread.currentThread().getName() + ": " + "Монитор " + lock1 + " захвачен");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + "Попытка захватить монитор " + lock2);
             synchronized (lock2){
-                System.out.println("Мониторы " + lock1 + lock2 + " захвачены");
+                System.out.println(Thread.currentThread().getName() + ": " + "Мониторы " + lock1 + lock2 + " захвачены");
             }
         }
     }
