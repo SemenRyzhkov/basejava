18787cdaceda50d012ef9dcbd81503a868c943bd
SemenRyzhkov <thaisun2018@yandex.ru>
2020-10-22 11:07:23 +0300

    HW12addJavaStreamEx

--- src/ru/javawebinar/basejava/JavaStreamsEx.java
+++ src/ru/javawebinar/basejava/JavaStreamsEx.java
@@ -6,23 +6,19 @@ import java.util.stream.Collectors;
 
 public class JavaStreamsEx {
     public static void main(String[] args) {
-//        int[] array = {9,8};
-//        System.out.println(minValue(array));
+        int[] array = {8, 1, 2, 3, 3, 2, 3, 7};
+        System.out.println(minValue(array));
 
-        List<Integer>list = List.of(2, 1);
+        List<Integer> list = List.of(2, 1, 1);
         System.out.println(oddOrEven(list));
     }
 
     public static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((x, y) -> Integer.parseInt(x + "" + y)).getAsInt();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> (x * 10) + y);
     }
 
-    public static List<Integer> oddOrEven(List<Integer> integers){
-        return integers.stream().filter(e->{
-            if (integers.stream().mapToInt(a->a).sum()%2==0){
-               return e%2==1;
-            }else return e%2==0;
-        }).collect(Collectors.toList());
-
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(a -> a).sum();
+        return integers.stream().filter(e -> sum % 2 == 0 ? e % 2 == 1 : e % 2 == 0).collect(Collectors.toList());
     }
 }
